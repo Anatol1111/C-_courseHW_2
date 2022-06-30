006a619b7f74ca619192f824e3c9490c00ac3158
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-06-30 19:18:12 +0600

    t_1 modiffied

--- tasks/t_1/Program.cs
+++ tasks/t_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+int num = int.Parse(Console.ReadLine());
+
+int TakeNum(int num)
+{
+    return (num / 10) % 10;
+}
+Console.WriteLine(TakeNum(num));
