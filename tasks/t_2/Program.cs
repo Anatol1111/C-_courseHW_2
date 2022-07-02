2a43e069b701fe29dcf7e3558622011c7d399c19
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-02 21:47:54 +0600

    t_2 modiffied

---
+++ tasks/t_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+
+void ThreeNum()
+{
+    int randomnumber = new Random().Next(100, 1000);
+    int firstNum = randomnumber / 100;
+    int thirdNum = randomnumber % 10;  
+    Console.WriteLine($"{randomnumber}-->{firstNum}{thirdNum}");
+}
+
+ThreeNum();
