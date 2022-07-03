8b2979bcd013cb8955071d1b7165b5cec16ff97e
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-03 10:28:50 +0600

    t_4 modiffied

---
+++ tasks/t_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 3: Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+void functionNum()
+{
+Console.Write("Введите число ");
+int Number = int.Parse(Console.ReadLine());
+if (Number >= 100)
+{
+    Console.WriteLine(Number % 10);
+}
+else
+{
+    Console.WriteLine($"{Number} --> Третьей цифры нет");
+}
+}
+
+functionNum();
---
+++ tasks/t_4/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 4: Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+void Aweek()
+{
+    Console.Write("Введите день недели ");
+    int Number = int.Parse(Console.ReadLine());
+    int Saturday = 6;
+    int Sunday = 7;
+    if (Number == Saturday)
+    {
+       Console.WriteLine("Выходной");
+    }
+   else if (Number == Sunday)
+    {
+       Console.WriteLine("Выходной");
+    }
+    else 
+    {
+       Console.WriteLine("Этот день не выходной"); 
+    }
+}
+Aweek();
+
